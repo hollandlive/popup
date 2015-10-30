2450c0499a2306ea88b1664552086b1cdabbfb39
Artem <dutchnewsru@gmail.com>
2015-10-30 08:03:27 +0100

    Create ecommerce_menu.js

---
+++ ecommerce_menu.js
@@ -0,0 +1,19 @@
+<script>
+$( document ).ready(function() {
+
+
+
+
+	$('#about').click(function() {
+		if ($('#aboutdiv').is(':hidden')) {
+			$('#aboutdiv').slideDown('slow');
+			
+		} else{ 
+			
+			$('#aboutdiv').slideDown('slow');
+		}
+	});
+});
+
+
+</script>
